f9acbf3b0442df65997d3fe675a7425007c61f79
MAC M1 PRO CHIP <a1234@Rupu.local>
2026-09-08 20:35:03 +0600

    Set up products and authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists.')
+            return redirect('register')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already exists.')
+            return redirect('register')
+
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        messages.success(request, 'Registration successful!')
+        return redirect('register')
+
+
+    return render(request, 'accounts/register.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful!')
+            return redirect('register')
+
+        messages.error(request, 'Invalid username or password.')
+        return redirect('login')
+
+    return render(request, 'accounts/login.html')
