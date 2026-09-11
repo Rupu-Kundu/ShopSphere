cdff078caa7cf60634fe4025b634d40f4aed6376
MAC M1 PRO CHIP <a1234@Rupu.local>
2026-09-11 15:16:38 +0600

    Add cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Cart, CartItem
+from products.models import Product
 
-# Create your views here.
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    cart_item, created = CartItem.objects.get_or_create(
+        cart=cart,
+        product=product
+    )
+
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect("cart")
+
+def cart_view(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    return render(request, "cart/cart.html", {
+        "cart": cart,
+    })
